a37c229ebb976cf81874f04a1a9af4ab679a24cd
espinosadiazaranza-coder <espinosadiazaranza@gmail.com>
2025-08-18 18:49:40 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
+      🚀 Mi Web funcionando en Vercel con Vite + React + TSX
+    </h1>
+  );
+}
+
+export default App;
